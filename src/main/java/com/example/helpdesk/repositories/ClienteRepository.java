e02a6755dff78dfae0217c57e64d9516e11048ba
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-11-26 23:21:57 -0300

    Adicionando os Repositorys Configurando e utilizando h2 e retirando import desnecessário

--- src/main/java/com/example/helpdesk/repositories/ClienteRepository.java
+++ src/main/java/com/example/helpdesk/repositories/ClienteRepository.java
@@ -1,7 +1,6 @@
 package com.example.helpdesk.repositories;
 
 import com.example.helpdesk.domain.enums.Cliente;
-import com.example.helpdesk.domain.enums.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
