a894e136a83ac9be251336a0034ac29e856dc1ee
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-11-30 16:21:36 -0300

    Criado findById para Chamado

---
+++ src/main/java/com/example/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.example.helpdesk.services;
+
+import com.example.helpdesk.domain.enums.Chamado;
+import com.example.helpdesk.repositories.ChamadoRepository;
+import com.example.helpdesk.services.exceptions.ObjectnotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository repository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID:" + id));
+
+    }
+}
